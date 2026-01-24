6e4e24b2720f4c8be781d47ce830f801733d24b8
brainiac <brainiac2k@gmail.com>
2026-01-23 20:10:59 -0800

    Move eqglib to eqglib folder, deprecated the zone-utilities project

---
+++ dependencies/zone-utilities/zone-utilities.cpp
@@ -0,0 +1,2 @@
+
+#error("zone-utilities is deprecated. If you see this message, it means you are using an old project structure. To build this project you should use the solution file provided by the MQ2Nav repository.")
