40f18bac92e3c02f6a2c189e8b00987a5be3d5b7
Knightly <knightly.cavalier@gmail.com>
2026-04-08 22:40:33 -1000

    Remove _WIN32_WINNT redefine

--- meshgen/targetver.h
+++ meshgen/targetver.h
@@ -1,9 +1,5 @@
 #pragma once
 
-// Including SDKDDKVer.h defines the highest available Windows platform.
-
-// If you wish to build your application for a previous Windows platform, include WinSDKVer.h and
-// set the _WIN32_WINNT macro to the platform you wish to support before including SDKDDKVer.h.
-#define  _WIN32_WINNT 0x600
+// _WIN32_WINNT is defined in Common.props
 
 #include <SDKDDKVer.h>
